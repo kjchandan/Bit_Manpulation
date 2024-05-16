7c05716b828ef60382180f74a5cb24eb8dc7fe49
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-16 18:03:43 +0530

    Rightmost setbit fidnign program

--- src/Bit_manipulation/Rightmost_set_bit.java
+++ src/Bit_manipulation/Rightmost_set_bit.java
@@ -8,8 +8,9 @@ public class Rightmost_set_bit {
 		int n= scan.nextInt();
 		
 		System.out.println((int)(Math.log10(n ^ (n &(n-1))) / Math.log10(2))+1);
- // (log  (n XOR (n AND n-1)  )
-// Find the position Soo fidn the 2 pow value and add 1 to it for the position.  //2 pow n => log2 n ......... log2 N =>  log10 N/ log10 2.
+		// (log  (n XOR (n AND n-1)  )
+		// Find the position Soo find the 2 pow value and add 1 to it for the position.  //2 pow n => log2 n.
+		//log2 N =>  log10 N/ log10 2.
 		scan.close();
 	}
 
--- src/Bit_manipulation/switch_on_i_bit.java
+++ src/Bit_manipulation/switch_on_i_bit.java
@@ -1,7 +1,8 @@
 package Bit_manipulation;
 
 public class switch_on_i_bit {
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		int n =36;
 		int i=3;
 		
